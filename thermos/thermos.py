f6a1357a3137674b0568197e4571b346dfe9c5cb
dmdv <dimos-d@yandex.ru>
2017-10-23 01:30:58 +0300

    Added flash

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,24 +1,64 @@
-from flask import Flask, render_template, url_for
+"""Main module"""
 
-app = Flask(__name__)
+from datetime import datetime
+from flask import Flask, render_template, url_for, request, redirect, flash
+from logging import DEBUG
 
-class User:
+SECRET_KEY = '\xfdq\xd7\x0b\xfa\x1c\xd3\xcd\x9fZ\x7f\x15\x8a\xfb\xed\xc6\xdf\xbc-\xd5\xff\xcbfl'
+
+APP = Flask(__name__)
+APP.secret_key = SECRET_KEY
+APP.config['SECRET_KEY'] = SECRET_KEY
+
+APP.logger.setLevel(DEBUG)
+
+bookmarks = []
+
+def store_bookmark(url):
+    """saves bookmark"""
+    bookmarks.append(
+        dict(
+            url=url,
+            user="dmitry",
+            date=datetime.utcnow()))
+
+class User(object):
+    """ User to save """
     def __init__(self, firstname, lastname):
         self.firstname = firstname
         self.lastname = lastname
 
     def initials(self):
+        """Initials"""
         return "{}. {}.".format(self.firstname[0], self.firstname[0])
 
-@app.route('/')
-@app.route('/index')
+@APP.route('/')
+@APP.route('/index')
 def index():
-    # by default looks in templates folder
-   return render_template("index.html", user=User("Dmitry", "Dyachkov"))
+    """by default looks in templates folder"""
+    return render_template("index.html", user=User("Dmitry", "Dyachkov"))
 
-@app.route('/add')
+@APP.route('/add', methods = ['GET', 'POST'])
 def add():
-   return render_template("add.html")
+    """adds urls"""
+    if request.method == "POST":
+        url = request.form['url']
+        save_bookmark(url)
+        return redirect(url_for('index'))
+    return render_template("add.html")
+
+@APP.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@APP.errorhandler(500)
+def page_not_found(e):
+    return render_template('404.html'), 500
+
+def save_bookmark(url):
+    store_bookmark(url)
+    flash('Stored url: ' + url)
+    APP.logger.debug('Stored url: ' + url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    APP.run(debug=True)
